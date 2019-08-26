74791ae71d597045c3ccd3ee19b5da4b112b1142
Mikhail <wot.smailik@yandex.ru>
2019-08-26 23:47:40 +0300

    Final version of test app

---
+++ app/src/main/java/donteco/testapp/audiohelper/ConstantsForApp.java
@@ -0,0 +1,28 @@
+package donteco.testapp.audiohelper;
+
+public class ConstantsForApp {
+
+    public static final int AUDIO_COUNT = 2;
+    public static final int STARTING_POSITION = 0;
+
+    public static final int CROSSFADE_STEP_AMOUNT = 100;
+    public static final int CROSSFADE_MAX_VOLUME = 1;
+
+    public static final int LENGTH_OF_AUDIO_INFO = 30;
+
+    public static final long STANDART_AUDIO_INTERVAL_MS = 1000;
+
+    public static final int MY_PERMISSION_REQUEST = 1;
+
+    public static final int standartAlbumIconId =  R.drawable.standart_album_icon;
+    public static final int leftChooseMusicIconId = R.id.iv_standart_music_icon_left;
+    public static final int rightChooseMusicIconId = R.id.iv_standart_music_icon_right;
+    public static final int chooseMusicIconId = R.drawable.choose_music_icon;
+    public static final int chooseMusicIconPressedId = R.drawable.choose_music_icon_pressed;
+    public static final int chooseMusicIconDisabledId = R.drawable.choose_music_icon_disabled;
+    public static final int playMusicIconId = R.drawable.crossfade_activity_play_icon;
+    public static final int pauseMisicIconId = R.drawable.crossfade_activity_pause_icon;
+    public static final int stopMisicIconId = R.drawable.crossfade_activity_stop_icon;
+    public static final int leftChooseMusicBtnId = R.id.img_btn_choose_first_song;
+    public static final int rightChooseMusicBtnId = R.id.img_btn_choose_second_song;
+}
---
+++ app/src/main/java/donteco/testapp/audiohelper/player/Crossfade.java
@@ -0,0 +1,120 @@
+package donteco.testapp.audiohelper.player;
+
+import android.media.MediaPlayer;
+
+import java.util.ArrayList;
+
+import donteco.testapp.audiohelper.ConstantsForApp;
+
+public class Crossfade implements Runnable
+{
+    private AudioPlayer audioPlayer;
+
+    private MediaPlayer curMediaPlayer;
+
+    private long curAudioDurationMS;
+    private long curAudioTimeMs;
+    private float curVolume;
+
+    private long crossFadeDurationMS;
+    private long crossFadeTimeIntervalMS;
+    private float crossFadeVolumeInterval;
+
+    public Crossfade(AudioPlayer audioPlayer, MediaPlayer curMediaPlayer, long crossFadeDurationMS, long crossFadeTimeIntervalMS, float crossFadeVolumeInterval)
+    {
+        this.audioPlayer = audioPlayer;
+        this.curMediaPlayer = curMediaPlayer;
+        this.crossFadeDurationMS = crossFadeDurationMS;
+        this.crossFadeTimeIntervalMS = crossFadeTimeIntervalMS;
+        this.crossFadeVolumeInterval = crossFadeVolumeInterval;
+
+        curAudioDurationMS = curMediaPlayer.getDuration();
+        curAudioTimeMs = 0;
+        curVolume = 0;
+    }
+
+    //Main function
+    //if mediaPlayer is trying to set smth on the released player, we catch IllegalStateException
+    //than, main loop
+    //first if - for fist crossFadeDurationMS
+    //second if - for last crossFadeDurationMS
+    //else - for interval after first crossFadeDurationMS and before last crossFadeDurationMS
+    @Override
+    public void run()
+    {
+        try
+        {
+            boolean needFadeOut = true;
+            while (curMediaPlayer.isPlaying())
+            {
+                try
+                {
+                    if(curAudioTimeMs <= crossFadeDurationMS)
+                        fadeIn();
+                    else
+                    {
+                        if((curAudioDurationMS - curAudioTimeMs) <= crossFadeDurationMS)
+                        {
+                            if (needFadeOut)
+                            {
+                                needFadeOut = false;
+                                playNextSong();
+                            }
+                            fadeOut();
+                        }
+                        else
+                            timeCounting();
+                    }
+                }
+                catch (InterruptedException e)
+                {
+                    System.out.println("Some thread interrupt \"crossFade\" thread!");
+                    stopPlayer();
+                    return;
+                }
+            }
+            stopPlayer();
+        }
+        catch (IllegalStateException e)
+        {
+            System.out.println("Player has been released");
+        }
+    }
+
+    private void fadeIn() throws InterruptedException
+    {
+        curVolume += crossFadeVolumeInterval;
+        curAudioTimeMs += crossFadeTimeIntervalMS;
+        curMediaPlayer.setVolume(curVolume, curVolume);
+        Thread.sleep(crossFadeTimeIntervalMS);
+    }
+
+    private void timeCounting() throws InterruptedException
+    {
+        curAudioTimeMs += ConstantsForApp.STANDART_AUDIO_INTERVAL_MS;
+        Thread.sleep(ConstantsForApp.STANDART_AUDIO_INTERVAL_MS);
+    }
+
+    private void fadeOut() throws InterruptedException
+    {
+        curVolume -= crossFadeVolumeInterval;
+        curAudioTimeMs += crossFadeTimeIntervalMS;
+        curMediaPlayer.setVolume(curVolume, curVolume);
+        Thread.sleep(crossFadeTimeIntervalMS);
+    }
+
+    private void playNextSong()
+    {
+        int songIndex = audioPlayer.getCurSongPlayingIndex();
+        songIndex = ++songIndex == ConstantsForApp.AUDIO_COUNT? 0 : songIndex;
+        audioPlayer.setCurSongPlayingIndex(songIndex);
+        audioPlayer.play(audioPlayer.getUriOfNextSong());
+    }
+
+    private void stopPlayer()
+    {
+        ArrayList<MediaPlayer> mediaPlayers = audioPlayer.getMediaPlayers();
+        mediaPlayers.remove(curMediaPlayer);
+        curMediaPlayer.release();
+    }
+}
